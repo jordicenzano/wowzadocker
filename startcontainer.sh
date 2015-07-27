4d84d0004646a4c914df00342a21dbd670231d1e
Jordi Cenzano <jcenzano@brightcove.com>
2015-07-27 10:32:08 -0700

    Protect container from files changes

--- startcontainer.sh
+++ startcontainer.sh
@@ -7,4 +7,4 @@ fi
 
 WOWZA_KEY=$(cat creds/wowzalicense)
 #With --rm all modification to the container files are deleted when exit
-docker run --name='wowzatest_container' -it -e "WOWZA_KEY=${WOWZA_KEY}" -p 2222:22 -p 1935:1935 -p 8086:8086 -p 8087:8087 -p 8088:8088 $1
+docker run --name='wowzatest_container' --rm -it -e "WOWZA_KEY=${WOWZA_KEY}" -p 2222:22 -p 1935:1935 -p 8086:8086 -p 8087:8087 -p 8088:8088 $1
